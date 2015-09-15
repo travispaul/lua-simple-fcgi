5e78e28ff0eb8a1ce511a60975c392e974adfcac
Travis Paul <Tr@visPaul.me>
2015-09-15 12:13:08 -0400

    Add cache example

--- examples/lua/page/main.lua
+++ examples/lua/page/main.lua
@@ -2,28 +2,40 @@ local util = require 'util'
 local lustache = require 'lustache'
 local markdown = require 'markdown'
 
-local fcgi = {}
+local app = {}
 local actions = {}
+local cache = {}
 local template
+local requests 
 
 function actions.GET(page)
-    local view = {page = page}
-    local text = util.read('pages/' .. page .. '.md') or
-       util.read('pages/404.md')
 
-    -- Due to unusual behavior with partials, we render twice
-    view.body = lustache:render(markdown(text), view)
-    -- XXX make test case, and file bug report if applicable
- 
-     return util.response(lustache:render(template, view))
+    if not cache[page] then
+        local view = {page = page, requests = requests}
+        local text = util.read('pages/' .. page .. '.md') or util.read('pages/404.md')
+
+        -- Due to unusual behavior with partials, we render twice
+        view.body = lustache:render(markdown(text), view)
+        -- XXX make test case, and file bug report if applicable
+    
+        cache[page] = util.response(lustache:render(template, view))
+    end
+    return cache[page]; 
 end
 
-function fcgi.start()
+function app.restart()
+    cache = {}
     template = util.read('layout.html')
     return 0
 end
 
-function fcgi.accept()
+function app.start()
+    app.restart()
+    requests = 0
+    return 0
+end
+
+function app.accept()
     local method = os.getenv('REQUEST_METHOD') or 'GET'
     local url = os.getenv('REQUEST_URI') or arg[1]
     local parts = util.split(url, '/')
@@ -38,13 +50,17 @@ function fcgi.accept()
         page = '404'
         method = 'GET'
     end
-    
+   
+    requests = requests + 1
+ 
     return actions[method](page)
 end
 
--- uncomment when testing or running as CGI
--- fcgi.start()
--- print(fcgi.accept())
---
+-- Testing/Debug
+-- app.start()
+-- print(app.accept())
+-- lua main.lua /page
+-- lua main.lua /page/home
+-- lua main.lua /page/foo
 
-return fcgi
+return app
